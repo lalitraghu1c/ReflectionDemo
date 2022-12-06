cf818476765048f0efecb055327c6471936d3f54
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-06 22:57:17 +0530

    [Lalit] Add: Folder Structure Created

---
+++ ReflectionDemo/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Customer
+    {
+        public int a, b;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Customer()
+        {
+            Id = 7;
+            Name = "Lalit"; 
+        }
+        public Customer(int id)
+        {
+            Id = id;
+        }
+        public void Print()
+        {
+
+        }
+        public void Test(int a)
+        {
+
+        }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReflectionDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Reflection Demo Problem Statement");
+            ReflectionTest.Test();
+        }
+    }
+}
